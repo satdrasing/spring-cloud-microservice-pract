7a97f4d04f33c6006eba8a76bf34bd16e0902b6f
satdrasing <satendra.kohli@gmail.com>
2020-02-20 01:46:27 +0530

    initial commit

---
+++ discovery-service/src/test/java/com/satendra/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satendra.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
