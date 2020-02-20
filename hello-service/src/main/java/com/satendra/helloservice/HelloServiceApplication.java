5bb08abeea79bc12b338f3c4ef6cc10337639c30
satdrasing <satendra.kohli@gmail.com>
2020-02-20 21:21:35 +0530

    error fixed

--- hello-service/src/main/java/com/satendra/helloservice/HelloServiceApplication.java
+++ hello-service/src/main/java/com/satendra/helloservice/HelloServiceApplication.java
@@ -3,11 +3,11 @@ package com.satendra.helloservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableEurekaClient
-//@EnableResourceServer
+@EnableResourceServer
 public class HelloServiceApplication {
 
 	public static void main(String[] args) {
--- uaa-service/src/main/java/com/satendra/uaaservice/ResourceServerConfigurer.java
+++ uaa-service/src/main/java/com/satendra/uaaservice/ResourceServerConfigurer.java
@@ -5,8 +5,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
-@Configuration
-@EnableResourceServer
+//@Configuration
+//@EnableResourceServer
 public class ResourceServerConfigurer extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
